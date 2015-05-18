b57688979b945919f0da51bc216ee6b5738dd227
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2015-05-18 23:24:33 +0300

    no get params

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require_once 'nmap.cls.php';
 $n=new WebMap('/usr/local/bin/nmap');
-$n->header();
-echo $n;
 $n->run_nmap();
-$n->footer();
+
 ?>
--- nmap.cls.php
+++ nmap.cls.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+
+ini_set("date.timezone", "America/New_York");
+
 class WebMap {
   private $args='';
   function __construct($cmd)  {
@@ -25,12 +28,8 @@ class WebMap {
       $this->tcp=$check;
       $this->method='post';
     }
-  $this->width="620px";
-  $this->tablebgcolor        = '#e1e1e1';
-  $this->hostsectioncolor    = '#913a47';
-  $this->scansectioncolor    = '#3c7996';
-  $this->generalsectioncolor = '#3a914b';
-  if ($this->submit && $this->host) 
+
+  if ($this->submit && $this->host)
   {
    $args = '';
    switch ($this->scan_type) 
@@ -129,80 +128,7 @@ class WebMap {
  function __set($name, $value) {
      $this->nmap[$name]=$value;
  }
- /**
-  * xHTML header, must run footer to colse the document 
-  */
- function header() {
-  if (stristr($_SERVER['HTTP_ACCEPT'], "application/xhtml+xml")) {
-   $this->mime="application/xhtml+xml";
-   header("Content-Type: ".  $this->mime);
-   print '<?xml version="1.0" encoding="utf-8"?>';
-  } else {
-   $this->mime="text/html";
-   header("Content-Type: ".  $this->mime);
-  }
-  echo <<<H
-<html xmlns="http://www.w3.org/1999/xhtml" >
- <head>
-  <title>$this->title</title>
-  <meta http-equiv="Content-Type" content="$this->mime; charset=utf-8" />
-  <meta http-equiv="Content-Language" content="en-us" />
-  <style type="text/css">
-  ul {
-            list-style-type: none; 
-            padding: 3px; 
-            margin: 2px; 
-  }         
-  .tablebgcolor { 
-            background-color: $this->tablebgcolor; 
-            width: $this->width; 
-            padding: 5px;
-            text-align: center; 
-  }
-  div.hostsectioncolor    { 
-            background-color: $this->hostsectioncolor;
-            width: $this->width; 
-            padding: 5px; 
-            text-align: center; 
-  }
-  input.host {
-            width: 100px;
-  }
-  input.button {
-            width: 40px;
-  }
-  div.scansectioncolor    { 
-            background-color: $this->scansectioncolor; 
-            height: 230px;
-            width: 200px; 
-            padding: 5px; 
-            float: left;
-  }
-  div.generalsectioncolor { 
-            background-color: $this->generalsectioncolor;
-            width: $this->width; 
-            padding: 5px; 
-            height: 230px;
-  }
-  div.generalsectioncolor div {
-            float: right;
-  }
-  h2 {
-            text-align: center; 
-  }
-  input.gen {
-            width: 110px;
-  }
-  </style>
- </head>
- <body>
-H
-    ;
- }
- 
- function footer() {
-  echo "</body></html>";
- }
+
  /**
   * run nMap
   * @param bool $log Logging off by default since windows mostlikely won't have the tee command
@@ -223,64 +149,7 @@ H
   * xHTML Form for nMap front end
   * @return string
   */
- function __toString() {
-    $page=$_SERVER['SCRIPT_NAME'];
-    return <<<HTML
- <form action="$page" method="$this->method">
- <div class="tablebgcolor" >
-  <h1>$this->title</h1>
- </div>
- <div class="hostsectioncolor">
-  <b>Хост:</b> 
-  <input class="host" type="text" name="host"  value="$this->host" />
-  <input class="button" type="submit" name="submit" value="scan"/>
-  <input class="button" type="reset" value="clear" />
- </div>
- <div  class="scansectioncolor">
-  <h2>Опции сканирования:</h2>
-  <ul>
-    <li><input type="radio" name="scan_type" value="connect" $this->connect /> connect()</li>
-    <li><input type="radio" name="scan_type" value="syn" $this->syn /> SYN Stealth </li>
-    <li><input type="radio" name="scan_type" value="null" $this->null /> NULL Scan </li>
-    <li><input type="radio" name="scan_type" value="fin" $this->fin /> FIN Scan</li>
-    <li><input type="radio" name="scan_type" value="xmas" $this->xmas /> XMAS Scan</li>
-    <li><input type="radio" name="scan_type" value="ack" $this->ack /> ACK Scan</li>
-    <li><input type="radio" name="scan_type" value="window" $this->window /> Window Scan</li>
-  </ul>         
- </div> 
- <div  class="generalsectioncolor" >
-  <h2>Остальны опции:</h2>
-  <div >
-   <ul> 
-    <li><input type="checkbox" name="use_port" $this->use_port /> Port Range:<br /> <input class="gen" type="text" name="port_range"   value='$this->port_range' /></li>
-    <li><input type="checkbox" name="use_decoy" $this->use_decoy /> Use Decoy(s):<br /> <input class="gen" type="text" name="decoy_name"  value='$this->decoy_name' /></li>
-    <li><input type="checkbox" name="use_device" $this->use_device /> Use Device:<br /> <input class="gen" type="text" name="device_name"  value='$this->device_name' /></li>
-   </ul>
-  </div>
-  <div >
-   <ul>
-    <li><input type="checkbox" name="dont_resolve" $this->dont_resolve /> Don't Resolve</li>
-    <li><input type="checkbox" name="fast_scan" $this->fast_scan /> Fast Scan</li>
-    <li><input type="checkbox" name="verbose" $this->verbose /> Verbose</li>
-    <li><input type="checkbox" name="udp_scan" $this->udp_scan /> UDP Scan</li>
-    <li><input type="checkbox" name="rpc_scan" $this->rpc_scan /> RPC Scan</li>
-    <li><input type="checkbox" name="fragmentation" $this->fragmentation /> Fragmentation</li>
-    <li><input type="checkbox" name="os_detect" $this->os_detect /> OS Detection</li>
-   </ul>
-  </div>            
-  <div>
-   <ul >
-    <li><b>Тип пинга:</b></li>
-    <li><input type="radio" name="ping_type" value="tcp" $this->tcp /> TCP Ping</li>
-    <li><input type="radio" name="ping_type" value="tcp_icmp" $this->tcp_icmp /> TCP&amp;ICMP Ping</li>
-    <li><input type="radio" name="ping_type" value="icmp" $this->icmp /> ICMP Ping</li>
-    <li><input type="radio" name="ping_type" value="none" $this->none /> Don't Ping</li>
-   </ul>
-  </div>
- </div>
-</form>
-HTML
-    ;
-  }
+
+
 }
 ?>
